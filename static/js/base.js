6b77320c1ec873fdd2889316c48fb53f4426b34c
mistlake <137457292+GlowingVoices@users.noreply.github.com>
2023-09-24 18:12:03 -0400

    Initial commit

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+
+{
+  let w_1 = document.querySelector("#v1");
+  let w_2 = document.querySelector("#v3");
+
+  let width = w_1.offsetWidth;
+  w_2.style.width = `${width}px`;
+}
---
+++ static/js/search.js
@@ -0,0 +1,78 @@
+
+const search_box = document.querySelector("#search_box");
+const search_button = document.querySelector("#search_button");
+const back_button = document.querySelector("#back_button");
+
+const news = document.querySelector("#news");
+
+news.addEventListener('click', () => {
+  hide_all_except(news);
+  setup_news();
+});
+
+function hide_all_except(except_me) {
+  const elementsToHide = document.querySelectorAll(".clicker");
+  elementsToHide.forEach(element => {
+    if(element !== except_me) {
+      element.style.display = "none";
+    }
+  });
+}
+
+function setup_news()
+{
+  let content = get_latest_rss();
+  display(content);
+}
+
+function get_latest_rss()
+{
+  fetchAndPrint();
+  //open up the .txt file and get the rss info
+  //actually grab the rss info from the net
+  //take the most popular rss stuff for each
+}
+
+const reader = new FileReader();
+
+function fetchAndPrint() {
+  alert('here');
+
+  const fs = require('fs')
+
+  fs.readFile('stuff.txt', (err, data) => {
+      if (err) throw err;
+
+      alert(data.toString());
+  })
+
+  alert('here too');
+}
+
+
+function display(stuff)
+{
+
+}
+
+
+
+
+search_button.addEventListener('click', () => {
+  do_stuff(search_box.value);
+});
+
+search_box.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    do_stuff(search_box.value);
+  }
+});
+
+function do_stuff(text_box_content) {
+  alert(text_box_content);
+}
+
+function get_rss()
+{
+
+}
