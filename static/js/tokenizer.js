6827a6edbd057307039794bc9d67573e9ef6ab15
glowingvoices <glowingvoices@gmail.com>
2024-04-17 21:40:36 -0400

    Implemented frequency counts
    The inverted index now tracks the frequency with which words appear in documents alongside the names of the documents the words appear in.

--- static/js/tokenizer.js
+++ static/js/tokenizer.js
@@ -9,18 +9,26 @@ var tokenize = new Tokenizer();
 
 /*
 tokenize(raw_document_text)
-This function returns a SET of the tokens present in the STRING document it is passed.
+This function returns a MAP of the tokens present in the STRING document it is passed.
+The MAP consists of (word, INTEGER frequency) pairs
 If there is an error, it will print the error message to console and return an empty SET
 */
 
 function tokenizer(raw_document) {
   try {
     let array_of_tokens = tokenize.words()(raw_document);
-    let set_of_tokens = new Set();
-    array_of_tokens.forEach(function (token) {
-      set_of_tokens.add(token.value);
+    let map_of_tokens = new Map();
+    array_of_tokens.forEach(function (token_object) {
+      if (map_of_tokens.has(token_object.value)) {
+        map_of_tokens.set(
+          token_object.value,
+          map_of_tokens.get(token_object.value) + 1,
+        );
+      } else {
+        map_of_tokens.set(token_object.value, 1);
+      }
     });
-    return set_of_tokens;
+    return map_of_tokens;
   } catch (err) {
     console.error(`Error tokenizing Markdown file`);
   }
@@ -29,9 +37,9 @@ function tokenizer(raw_document) {
 /*
 get_token_set_from_one_document(path_to_document, document_name)
 This function accepts the STRING path to a document relative to the directory, and the STRING document name
-If the document does not exist, it will return an empty SET.
-If the document is not a markdown document, it will return an empty SET.
-If the document exists and is a markdown document, it will return the SET of the tokens.
+If the document does not exist, it will return an empty MAP.
+If the document is not a markdown document, it will return an empty MAP.
+If the document exists and is a markdown document, it will return the MAP of (token, INTEGER frequency).
 */
 function get_token_set_from_one_document(path_to_document, document_name) {
   if (path.extname(document_name) === ".md") {
@@ -42,28 +50,31 @@ function get_token_set_from_one_document(path_to_document, document_name) {
         "utf-8",
       );
 
-      //get SET of tokens from file
-      let set_of_tokens = tokenizer(file_contents);
-      return set_of_tokens;
+      //get MAP of tokens from file
+      let map_of_tokens = tokenizer(file_contents);
+      return map_of_tokens;
     } catch (err) {
       console.error(
         `Error reading Markdown file ${document_name} at ${path.join(path_to_document, document_name)}:`,
       );
     }
-    return new Set();
+    return new Map();
   }
-  return new Set();
+  return new Map();
 }
 
 function construct_index(everything_maps) {
   let inverted_index = new Map();
-  everything_maps.forEach(function (set_of_tokens, fileName) {
-    set_of_tokens.forEach(function (token) {
+  everything_maps.forEach(function (map_of_tokens, fileName) {
+    map_of_tokens.forEach(function (frequency, token) {
       if (inverted_index.has(token)) {
-        inverted_index.set(token, inverted_index.get(token).add(fileName));
+        inverted_index.set(
+          token,
+          inverted_index.get(token).add([fileName, frequency]),
+        );
       } else {
         let blank_set = new Set();
-        blank_set.add(fileName);
+        blank_set.add([fileName, frequency]);
         inverted_index.set(token, blank_set);
       }
     });
